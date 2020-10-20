0c43d9d47d3da4d3a21f2180b3f886f1fb5a1106
Basavaraj Kashetti <bkashetti@rsamindia.local>
2020-10-20 18:21:52 +0530

    Save111

--- TerraformDemo/Main.tf
+++ TerraformDemo/Main.tf
@@ -1,4 +1,16 @@
 resource "highbond_collection" "main"{
+    name = "Test1"
+    Description = "Demo repo test"
+    
+} 
+
+resource "highbond_collection" "main"{
+    name = "Test2"
+    Description = "Demo repo test"
+    
+} 
+
+resource "highbond_collection" "main"{
     name = "Test_Collection"
     Description = "Demo repo test"
     
